be9c105c125be9d64094e94d4d80d221ad4ecd6f
HukkeJlb <hukkejlb@gmail.com>
2018-11-25 10:34:50 +0300

    homework 2.2 - done

---
+++ #2/task-2/app.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const path = require("path");
+const newFolder = path.join(__dirname, "./newFolder");
+const sourceFolder = path.join(__dirname, "./source");
+const util = require("util");
+const copyAsync = util.promisify(fs.copyFile);
+
+if (!fs.existsSync(newFolder)) {
+  fs.mkdirSync(newFolder);
+}
+
+const sortingFiles = (base, level) => {
+  const content = fs.readdirSync(base);
+
+  content.forEach(item => {
+    let localBase = path.join(base, item);
+    let state = fs.statSync(localBase);
+    if (state.isDirectory()) {
+      sortingFiles(localBase, level + 1);
+    } else {
+      copyFile(item, localBase);
+    }
+  });
+};
+
+const copyFile = (file, filePath) => {
+  let firstFileLetter = file.substr(0, 1).toUpperCase();
+  let destinationFolder = path.join(newFolder, firstFileLetter);
+  let destinationPath = path.join(destinationFolder, file);
+
+  if (!fs.existsSync(destinationFolder)) {
+    fs.mkdirSync(destinationFolder);
+  }
+
+  fs.readFile(filePath, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.log("Error reading the file");
+      process.exit(1);
+    }
+
+    var fileExistance = fs.existsSync(destinationPath);
+
+    if (fileExistance) {
+      var destinationPathNew = path.join(destinationFolder, generateHash(file));
+    } else {
+      var destinationPathNew = path.join(destinationFolder, file);
+    }
+    copyAsync(filePath, destinationPathNew).then(() => {
+        console.log(`Файл ${filePath} успешно копирован`);
+    })
+    .catch((error) => {
+        console.log('Error: ', error)
+    });
+  });
+};
+
+const generateHash = filename => {
+  var filenameArray = stringSplice(filename);
+  var splicedFilename = filenameArray[0] + "_";
+  var extension = filenameArray[1];
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++) {
+    splicedFilename += possible.charAt(
+      Math.floor(Math.random() * possible.length)
+    );
+  }
+
+  var newFilename = splicedFilename + extension;
+
+  return newFilename;
+};
+
+const stringSplice = string => {
+  var extension = path.extname(string);
+  var withoutExt = path.parse(string).name;
+
+  return [withoutExt, extension];
+};
+
+sortingFiles(sourceFolder, 0);
